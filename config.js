5e4bd6188e07c44026380d297e4d478779bd496e
oaxsun <oaxsuntechnologies@gmail.com>
2026-05-08 16:33:51 -0600

    key ue config.js

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 export const APP_CONFIG = {
   SUPABASE_URL: "https://hgteuswezxhxtjhwwnkg.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_0FQaBGI448upKh7w_7Q81w_CRyxLid5",
+
+  // Stripe Checkout Function
   CREATE_CHECKOUT_FUNCTION_URL: "",
+
+  // Stripe Publishable Key
+  STRIPE_PUBLIC_KEY: "pk_test_51Sz3xRK5nFoesXlk0TKUH0JkIrjFAqdMUIAFOETiIJhQVCEbIrndwmneVu7XkLhRPPIq49qE2emc3Fcy433cvHpG006293PgzZ",
 
   // Stripe Price IDs - Compresso Pro
   STRIPE_MONTHLY_PRICE_ID: "price_1TUwUZK5nFoesXlkApbFtwEF",
